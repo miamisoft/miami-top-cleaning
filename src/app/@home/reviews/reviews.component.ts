00117f3e905712a38fde844197fac91354f4c833
alexclavijo <alexclavijo85@gmail.com>
2019-01-24 12:23:16 -0500

    updating media url

--- src/app/@home/reviews/reviews.component.ts
+++ src/app/@home/reviews/reviews.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef, AfterViewInit, ChangeDetectionStrategy } from '@angular/core';
 import { DataService } from 'src/app/core/data.service';
+import { environment } from 'src/environments/environment';
 
 interface ReviewsData{
   yelpCodeSnippet: string;
@@ -14,10 +15,12 @@ interface ReviewsData{
 export class ReviewsComponent  implements OnInit {
 
   public reviews: ReviewsData;
+  public env: any;
   
   constructor(private _dataService: DataService, private changeDetector: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+    this.env = environment;
     this._dataService.getData('reviews').subscribe((res: ReviewsData) => {
       this.reviews = res;
    }, error => console.log(error));
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,12 @@ export const environment = {
   production: false,
   defaultLan: 'en',
   cookieLan: 'MIA-TOP-CL-LANG',
-  dataBaseUrl: '/assets/data'
+  dataBaseUrl: '/assets/data',
+  facebookUrl: 'https://www.facebook.com/miamitopcleaning/',
+  googleMapUrl: 'https://www.google.com/maps/place/Miami+Top+Cleaning+Service/@25.748514,-80.312378,14z/data=!4m5!3m4!1s0x0:0x701da3604f8bc3c9!8m2!3d25.748514!4d-80.312378?hl=en',
+  yelpUrl: 'https://www.yelp.com/biz/miami-top-cleaning-service-miami',
+  youtubeUrl: 'https://www.youtube.com/channel/UCzE6m0YERbTadS-Nr_1UcgQ',
+  twitterUrl: 'https://twitter.com/miamitopcleanin'
 };
 
 /*
