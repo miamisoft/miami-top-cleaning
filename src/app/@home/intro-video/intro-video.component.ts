de20a65fbbe3082de6a86f6de18ccfd0426ab534
alexclavijo <alexclavijo85@gmail.com>
2019-01-24 23:21:57 -0500

    banner images

--- src/app/@home/intro-video/intro-video.component.ts
+++ src/app/@home/intro-video/intro-video.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { DataService } from 'src/app/core/data.service';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
+
+interface VideoIntroData {
+  imgUrl: string;
+  question: string;
+  
+  slogan: string;
+  desc: string;
+}
 
 @Component({
   selector: 'app-home-intro-video',
   templateUrl: './intro-video.component.html',
-  styleUrls: ['./intro-video.component.scss']
+  styleUrls: ['./intro-video.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class IntroVideoComponent implements OnInit {
+export class IntroVideoComponent implements AfterViewInit {
 
-  constructor() { }
+  public data: VideoIntroData[] = [];
 
-  ngOnInit() {
+  constructor(private _dataService: DataService, private sanitizer: DomSanitizer) {}
+
+  ngAfterViewInit(): void {
+    this._dataService.geti18nData('videos').subscribe((res: VideoIntroData[]) => {
+      this.data = res;
+   }, error => console.log(error));
+  }
+
+  public backgroundImgUrl(imageUrl: string): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(`url(${imageUrl})`);
   }
 
 }
--- src/app/@home/intro/intro.component.ts
+++ src/app/@home/intro/intro.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import { DataService } from 'src/app/core/data.service';
 import { AppScripts } from 'src/app/core/app.scripts';
+import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 
 interface IntroData {
     imgUrl: string;
@@ -12,13 +13,15 @@ interface IntroData {
 @Component({
   selector: 'app-home-intro',
   templateUrl: './intro.component.html',
-  styleUrls: ['./intro.component.scss']
+  styleUrls: ['./intro.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class IntroComponent implements AfterViewInit {
   
   public data: IntroData[] = [];
 
-  constructor(private _dataService: DataService) { }
+  constructor(private _dataService: DataService, 
+              private sanitizer: DomSanitizer) { }
 
   ngAfterViewInit(): void {
     this._dataService.geti18nData('intro').subscribe((res: IntroData[]) => {
@@ -27,6 +30,10 @@ export class IntroComponent implements AfterViewInit {
         AppScripts.setupFlexSlider();
       });
    }, error => console.log(error));
+  }
+
+  public backgroundImgUrl(imageUrl: string): SafeStyle {
+    return this.sanitizer.bypassSecurityTrustStyle(`linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),url(${imageUrl})`);
   }
 
 }
