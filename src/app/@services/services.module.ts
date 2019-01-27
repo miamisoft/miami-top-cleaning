31195cbf722543c89bb2f975886772291368a693
alexclavijo <alexclavijo85@gmail.com>
2019-01-27 16:49:17 -0500

    refactoring

--- src/app/@services/services.module.ts
+++ src/app/@services/services.module.ts
@@ -1,23 +1,37 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { servicesRoutes } from './services.routes';
-import { CommercialModule } from './@commercial/commercial.module';
-import { ResidentialModule } from './@residential/residential.module';
-import { WindowsModule } from './@windows/windows.module';
+
 import { PressureComponent } from './pressure/pressure.component';
 import { PostConstructionComponent } from './post-construction/post-construction.component';
 import { SharedModule } from '../@shared/shared.module';
+import { OfficeComponent } from './commercial/office/office.component';
+import { FloorComponent } from './commercial/floor/floor.component';
+import { DustRemovalComponent } from './commercial/dust-removal/dust-removal.component';
+import { JanitorialComponent } from './commercial/janitorial/janitorial.component';
+import { HouseComponent } from './residential/house/house.component';
+import { AppartmentComponent } from './residential/appartment/appartment.component';
+import { MoveComponent } from './residential/move/move.component';
+import { DeepComponent } from './residential/deep/deep.component';
+import { WindowComponent } from './windows/window/window.component';
+import { HighRiseWindowComponent } from './windows/high-rise-window/high-rise-window.component';
 
 @NgModule({
   imports: [
-    CommercialModule,
-    ResidentialModule,
-    WindowsModule,
     SharedModule,
     RouterModule.forChild(servicesRoutes)
   ],
   declarations: [
+    OfficeComponent, 
+    FloorComponent, 
+    DustRemovalComponent, 
+    JanitorialComponent,
+    HouseComponent, 
+    AppartmentComponent, 
+    MoveComponent, 
+    DeepComponent,
+    WindowComponent, 
+    HighRiseWindowComponent,
     PressureComponent,
     PostConstructionComponent
   ]
--- src/app/@services/services.routes.ts
+++ src/app/@services/services.routes.ts
@@ -1,18 +1,85 @@
 import { Routes } from "@angular/router";
-import { commercialRoutes } from "./@commercial/commercial.routes";
-import { residentialRoutes } from "./@residential/residential.routes";
-import { windowsRoutes } from "./@windows/windows.routes";
 import { PostConstructionComponent } from "./post-construction/post-construction.component";
 import { PressureComponent } from "./pressure/pressure.component";
+import { DustRemovalComponent } from "./commercial/dust-removal/dust-removal.component";
+import { FloorComponent } from "./commercial/floor/floor.component";
+import { JanitorialComponent } from "./commercial/janitorial/janitorial.component";
+import { OfficeComponent } from "./commercial/office/office.component";
+import { AppartmentComponent } from "./residential/appartment/appartment.component";
+import { HouseComponent } from "./residential/house/house.component";
+import { MoveComponent } from "./residential/move/move.component";
+import { DeepComponent } from "./residential/deep/deep.component";
+import { WindowComponent } from "./windows/window/window.component";
+import { HighRiseWindowComponent } from "./windows/high-rise-window/high-rise-window.component";
 
 
 export const servicesRoutes: Routes = [
 
-     ... commercialRoutes,
+    {
+        path: 'commercial',
+        children: [
+            {
+                path: 'high-level-dust-removal',
+                component: DustRemovalComponent
+            },
+            {
+               path: 'floor-cleaning',
+               component: FloorComponent
+           },
+       
+           {
+               path: 'janitorial-services',
+               component: JanitorialComponent
+           },
+       
+           {
+               path: 'office-cleaning',
+               component: OfficeComponent
+           }
+       
+           
+        ]
+    },
 
-     ... residentialRoutes,
+    {
+        path: 'residential',
+        children: [
+            {
+                path: 'appartment-cleaning',
+                component: AppartmentComponent,
+            },
+            {
+               path: 'house-cleaning',
+               component: HouseComponent
+           },
+       
+           {
+               path: 'movein-moveout-cleaning',
+               component: MoveComponent
+           },
+           
+           {
+               path: 'deep-cleaning',
+               component: DeepComponent
+           }
+        ]
+    },
 
-     ... windowsRoutes,
+    {
+        path: 'windows',
+        children: [
+            {
+                path: 'window-cleaning',
+                component: WindowComponent
+            },
+            
+            {
+                 path: 'high-rise-window-cleaning',
+                 component: HighRiseWindowComponent
+             }
+        ]
+    },
+   
 
     {
         path: 'post-construction-cleaning',
